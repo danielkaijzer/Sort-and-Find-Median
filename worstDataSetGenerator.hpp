c3cdbf443cf360ef1811346db76d3a89989faecc
danielkaijzer <danielkaijzer00@gmail.com>
2023-11-20 23:22:12 -0500

    First attempt at generateWorstDataSet function for exploiting weakness in Median of 3 pivot selection algorithm

---
+++ worstDataSetGenerator.hpp
@@ -0,0 +1,28 @@
+#ifndef WORST_DATA_SET_GENERATOR
+#define WORST_DATA_SET_GENERATOR
+
+#include <iostream>
+#include <random>
+
+std::vector<int> generateWorstDataSet(int size, int min, int max){
+    std::random_device rd;
+    std::mt19937 rng(rd());
+    std::uniform_int_distribution<int> dist(min, max);
+
+    std::vector<int> data;
+
+    data.push_back(max); // max will be first element
+
+    for (int i = 1; i < size-1; ++i){
+
+        data.push_back(dist(rng));
+    }
+
+    data[size/2] = max; // make median index the max value
+
+    data.push_back(max); // max will be last element
+
+    return data;
+}
+
+#endif
